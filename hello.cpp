4976b542981446ac51f2842c31c2e8868d04f350
XinLu <609988648@qq.com>
2017-09-24 23:03:29 +0800

    hello world

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "hello world!" << endl;
+    cout << "hello !" << endl;
+    printf("xxx");
+    int a = 0;
+    for(int i = 0; i < 10; i++)
+    {
+        a += i;
+    }
+
+    float d_num;
+    cout << sizeof(d_num) << endl;
+
     return 0;
 }
